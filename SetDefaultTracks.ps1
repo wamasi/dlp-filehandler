24b44e9a8c3f61e850b3d708533e8b9032e8f6f6
wamasi <mrwamasi@gmail.com>
2022-02-13 23:21:58 -0600

    removed extra scripts

--- SetDefaultTracks.ps1
+++
@@ -1,8 +0,0 @@
-$Path = ""
-Get-ChildItem $Path | Where-Object { $_.Extension -eq '.mkv' } | ForEach-Object {
-    Write-Host $_.FullName
-    # mkvpropedit $_.FullName --edit track:a2 --set flag-default=1
-    # mkvpropedit $_.FullName --edit track:a1 --set flag-default=0
-    # mkvpropedit $_.FullName --edit track:s2 --set flag-default=1
-    # mkvpropedit $_.FullName --edit track:s1 --set flag-default=0
-}
